feaa3d53f7bb2f2a1c617d67b115cc11142f44d0
krawcts <lucassampaio22k@gmail.com>
2024-07-02 22:19:49 -0300

    create etl file and load env variables

---
+++ app/etl.py
@@ -0,0 +1,21 @@
+import os
+from pathlib import Path
+
+import pandas as pd
+import pandera as pa
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+def load_settings():
+    """Loads settings from environment variables."""
+    dotenv_path = Path.cwd() / '.env'
+    load_dotenv(dotenv_path=dotenv_path)
+
+    settings = {
+        "db_host": os.getenv("POSTGRES_HOST"),
+        "db_user": os.getenv("POSTGRES_USER"),
+        "db_pass": os.getenv("POSTGRES_PASSWORD"),
+        "db_name": os.getenv("POSTGRES_DB"),
+        "db_port": os.getenv("POSTGRES_PORT"),
+    }
+    return settings
